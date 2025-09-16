a199f55324a0cab90f911fa4bd9a4067bd363d22
Leonidas Pispiringas <lpispir@gmail.com>
2025-09-16 15:00:54 +0300

    Update conf.py
    updating to cover Sphinx >= 8

--- docs/conf.py
+++ docs/conf.py
@@ -57,8 +57,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'OpenAIRE Guidelines for CRIS Managers'
-copyright = u'2018,2023, OpenAIRE. This work is licensed under Creative Commons Attribution 4.0 International'
+project = 'OpenAIRE Guidelines for CRIS Managers'
+copyright = '2018,2023, OpenAIRE. This work is licensed under Creative Commons Attribution 4.0 International'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -212,8 +212,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'OpenAIREGuidelines.tex', u'OpenAIRE Guidelines for CRIS Managers',
-   u'OpenAIRE + euroCRIS', 'manual'),
+  ('index', 'OpenAIREGuidelines.tex', 'OpenAIRE Guidelines for CRIS Managers',
+   'OpenAIRE + euroCRIS', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -242,8 +242,8 @@ latex_logo = '_static/openaire+eurocris.png'
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'openaireguidelines', u'OpenAIRE Guidelines for CRIS Managers',
-     [u'OpenAIRE + euroCRIS'], 1)
+    ('index', 'openaireguidelines', 'OpenAIRE Guidelines for CRIS Managers',
+     ['OpenAIRE + euroCRIS'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -256,8 +256,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'OpenAIREGuidelines', u'OpenAIRE Guidelines for CRIS Managers',
-   u'OpenAIRE + euroCRIS', 'OpenAIREGuidelines', 'One line description of project.',
+  ('index', 'OpenAIREGuidelines', 'OpenAIRE Guidelines for CRIS Managers',
+   'OpenAIRE + euroCRIS', 'OpenAIREGuidelines', 'One line description of project.',
    'Miscellaneous'),
 ]
 
@@ -275,4 +275,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+# OLD Sphinx < 8: intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
